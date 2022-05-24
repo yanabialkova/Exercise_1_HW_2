db46ed471ccccd71ce7c39784df8f524da127177
Yana Bialkova <yana.bialkova@gmail.com>
2022-05-24 19:00:08 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int tickets = 3000;
+        int spendMany = 20;
+        int mile = tickets / spendMany;
+        System.out.println("You are earned: " + mile + " mile");
+    }
+}
